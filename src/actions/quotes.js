2361054985f0906e694ca2e79544354afd0904e2
Kevin Huang <yellow.khaos@gmail.com>
2019-02-09 19:00:15 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -13,9 +13,16 @@ export const removeQuote = quoteId => {
   }
 }
 
-export const upvoteQuote = quoteId = {
+export const upvoteQuote = quoteId => {
   return {
     type: "UPVOTE_QUOTE",
+    quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: "DOWNVOTE_QUOTE",
     quoteId
   }
 }
