1652535b846ef9e4de68b9f4267b347f51475596
Kevin Huang <yellow.khaos@gmail.com>
2019-02-09 19:34:54 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -7,7 +7,7 @@ export default function quotes(state=[], action){
       return [...state, action.quote]
 
     case "RMOVE_QUOTE":
-      idx = state.indexOf(action.id)
+      idx = state.indexOf(action.quoteId)
       return [...state.slice(0,idx),...state.slice(idx+1)]
     default:
       return state
