0f6fa9af27ea16eb4d3e0f23139bd865180f54dc
Kevin Huang <yellow.khaos@gmail.com>
2019-02-09 19:33:52 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -5,6 +5,10 @@ export default function quotes(state=[], action){
   switch(action.type) {
     case "ADD_QUOTE":
       return [...state, action.quote]
+
+    case "RMOVE_QUOTE":
+      idx = state.indexOf(action.id)
+      return [...state.slice(0,idx),...state.slice(idx+1)]
     default:
       return state
   }
